f78cf93dec69f839ef986fbb56f63c3ae46244ef
zayminmaw <zayminmaw77@gmail.com>
2025-01-02 16:11:47 +0700

    feat: added prompt condition for window

--- prompt.c
+++ prompt.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+
 #include <editline/readline.h>
+
+#endif
 
 int main(int argc, char** argv)
 {
